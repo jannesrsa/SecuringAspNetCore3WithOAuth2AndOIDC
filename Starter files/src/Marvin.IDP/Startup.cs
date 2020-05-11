ae0cb0237795e8c7c0be342a17ffd69f3fea4f1b
Jannes Coetzee <jannesrsa@gmail.com>
2020-05-11 09:52:14 +0200

    Add SigningCredentials

--- Starter files/src/Marvin.IDP/Startup.cs
+++ Starter files/src/Marvin.IDP/Startup.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using System;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -47,7 +49,26 @@ namespace Marvin.IDP
                 .AddTestUsers(TestUsers.Users);
 
             // not recommended for production - you need to store your key material somewhere secure
-            builder.AddDeveloperSigningCredential();
+            //builder.AddDeveloperSigningCredential();
+            builder.AddSigningCredential(LoadCertificateFromStore());
+        }
+
+        public X509Certificate2 LoadCertificateFromStore()
+        {
+            string thumbPrint = "ee64846f6f51ee842c94ce4b442cd89e695ecbc4";
+
+            using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certCollection = store.Certificates.Find(X509FindType.FindByThumbprint,
+                    thumbPrint, true);
+                if (certCollection.Count == 0)
+                {
+                    throw new Exception("The specified certificate wasn't found.");
+                }
+
+                return certCollection[0];
+            }
         }
     }
 }
